dece08bbaf9b306877a3090bc68501e93c759622
JoshB2699 <joshb2699@gmail.com>
2017-03-21 11:47:08 +0000

    21/13/2017

--- server/public/js/clock.js
+++
@@ -1,8 +0,0 @@
-module.exports = function GetClock(){
-  var d=new Date();
-  var nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
-  if(nyear<1000) nyear+=1900;
-  var nhour=d.getHours(),nmin=d.getMinutes();
-  if(nmin<=9) nmin="0"+nmin;
-  document.getElementById('clockbox').innerHTML=""+ndate+"/"+(nmonth+1)+"/"+nyear+" "+nhour+":"+nmin+"";
-};
